66037abe9a369c909f8c5a9f8078e66c887f3b92
Jesicha hety manika <75687131+jesichamanika06@users.noreply.github.com>
2020-12-19 22:01:36 +0800

    Halaman dashboard

---
+++ index.php
@@ -0,0 +1,17 @@
+<html>
+<head>
+    <title>Form Login Admin</title>
+</head>
+<body>
+    <h2>Halaman Admin</h2>
+    <?php 
+        session_start();
+        if($_SESSION['status']!="login"){
+            header("location:../index.php?pesan=belum_login");
+        }
+    ?>
+    <h4>Selamat datang, <?php echo $_SESSION['username']; ?>! anda telah login.</h4>
+    <br/>
+    <a href="logout.php">LOGOUT</a>
+</body>
+</html>
