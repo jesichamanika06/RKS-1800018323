b73778e9de3b9238a2d17eb70427b32fa74bb2ab
Jesicha hety manika <75687131+jesichamanika06@users.noreply.github.com>
2020-12-19 21:58:02 +0800

    Form Login

--- index.php
+++ index.php
@@ -1,17 +1,43 @@
 <html>
 <head>
-    <title>Form Login Admin</title>
+    <title>Portal Akademik</title>
+    <link rel="stylesheet" type="text/css" href="body.css">
 </head>
 <body>
-    <h2>Halaman Admin</h2>
-    <?php 
-        session_start();
-        if($_SESSION['status']!="login"){
-            header("location:../index.php?pesan=belum_login");
+    <h1>PORTAL MAHASISWA</h1>
+    <h2>UNIVERSITAS AHMAD DAHLAN</h2>    
+    <form method="POST" action="login.php">
+        <div class="kotak_login">
+            <left class="tulisan_login">Login Portal</left> <br><br>
+    
+        <form>
+            <input type="text" name="username" class="form_login" placeholder="Username">
+
+            <input type="text" name="password" class="form_login" placeholder="Password">
+
+            <left>
+                <p><input type='checkbox' name='peringatan' value='php' />Ingatkan saya !</p>
+            </left>
+
+            <center>
+                <input type="submit" class="tombol_login" value="LOGIN">
+            <br>
+         <?php 
+        if(isset($_GET['pesan'])){
+            if($_GET['pesan'] == "gagal"){
+                echo "Login gagal! username dan password salah!";
+            }
+            else if($_GET['pesan'] == "logout"){
+                echo "Anda telah berhasil logout";
+            }
+            else if($_GET['pesan'] == "belum_login"){
+                echo "Anda harus login untuk mengakses halaman admin";
+            }
         }
-    ?>
-    <h4>Selamat datang, <?php echo $_SESSION['username']; ?>! anda telah login.</h4>
-    <br/>
-    <a href="logout.php">LOGOUT</a>
+    ?></center>
+        </form>
+        
+    </div>    
+    </form>
 </body>
 </html>
